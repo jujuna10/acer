04bc4bcb350cf9420a1c9354af8263106cb69aec
niko zhuzhunadze <jujunaniko@gmail.com>
2024-04-06 17:19:27 +0400

    project

---
+++ script.js
@@ -0,0 +1,9 @@
+const image = document.getElementById("hero-image")
+function changeImage(){
+    image.src = "./assets/01dlH1lPLKAD01ORH0LnMT2-1..v1659054385.jpg"
+}
+
+const image2 = document.getElementById("hero-image")
+function changeImage2(){
+    image2.src = "./assets/Acer_Aspire3-4.webp"
+}
